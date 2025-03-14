3b9e725664d8420528d895b43d9551c151c0ea0c
kirimine170 <kirimine170@gmail.com>
2025-03-14 21:49:15 +0900

    :memo: add AssemblyInfo.cs
    最低限入れただけ

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1df5060b-98e0-4b0b-95eb-81a178ebd7fe")]
+[assembly: MelonInfo(typeof(Program), "PlayerRECMod", "0.1.0", "kirimine170")]
+[assembly: MelonGame("PrimitierDev", "Primitier")]
