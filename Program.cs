347abc9c5614f596193eab463bd755077e14a8aa
kirimine170 <kirimine170@gmail.com>
2025-03-14 21:50:33 +0900

    :pencil2: update Program.cs
    ビルドだけ通るように

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MelonLoader;
+// See https://aka.ms/new-console-template for more information
+public class Program : MelonMod
+{
+    
+}
